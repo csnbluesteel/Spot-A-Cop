92512ce9aa20edcd270627264ac28eb93e41b18e
Marco Cruz-Martinez <cruzmart@unlv.nevada.edu>
2019-04-04 16:37:39 -0700

    I added the labels, now I'm working on the signup code

---
+++ Spot-A-Cop/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  Spot-A-Cop
+//
+//  Created by Marco Cruz on 4/4/19.
+//  Copyright © 2019 BLUESTEEL. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
