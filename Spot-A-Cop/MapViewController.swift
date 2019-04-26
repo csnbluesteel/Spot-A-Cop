e1f80a67ee835cf3cbf0cbe3979b06f6bc80ee3f
Tomas Nunez <TomasNunez@tomass-MacBook-Air.local>
2019-04-25 18:35:19 -0700

    dropping piggies

--- Spot-A-Cop/MapViewController.swift
+++ Spot-A-Cop/MapViewController.swift
@@ -2,48 +2,112 @@
 //  MapViewController.swift
 //  Spot-A-Cop
 //
-//  Created by tomas nunez on 4/4/19.
+//  Created by tomas nunez on 4/11/19.
 //  Copyright © 2019 BLUESTEEL. All rights reserved.
 //
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
-    @IBOutlet weak var Map: MKMapView!
+    @IBOutlet weak var mapView: MKMapView!
     
-    var locationManager: CLLocationManager!
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 300
+    
+    
+    
+    @IBAction func dropApin(_ sender: Any) {
+        
+        let annotation = MKPointAnnotation()
+        let locationCoordinate = CLLocationCoordinate2D(latitude: mapView.userLocation.coordinate.latitude, longitude: mapView.userLocation.coordinate.longitude)
+        annotation.coordinate = locationCoordinate
+        annotation.title = "COP!"
+        mapView.addAnnotation(annotation)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+    {
+        if !(annotation is MKPointAnnotation) {
+            return nil
+        }
+        
+        let annotationIdentifier = "annotation"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView!.canShowCallout = true
+        }
+        else {
+            annotationView!.annotation = annotation
+        }
+        annotationView!.image = UIImage(named: "Mr piggy")
+        annotationView?.transform = CGAffineTransform(scaleX: 0.08, y: 0.1)
+        return annotationView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if (CLLocationManager.locationServicesEnabled()){
-            locationManager = CLLocationManager()
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
-            locationManager.requestAlwaysAuthorization()
-            locationManager.startUpdatingLocation()
+        checkLocationServices()
+        mapView.delegate = self
+    }
+    
+    func setupLocationManager()  {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func centerViewOnUserLoction(){
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
         }
-        // Do any additional setup after loading the view.
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [AnyObject]!) {
-        let location = locations.last as! CLLocation
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, span:MKCoordinateSpan(latitudeDelta: 200, longitudeDelta: 200))
-        self.Map.setRegion(region, animated: true)
+    func checkLocationServices(){
+        if CLLocationManager.locationServicesEnabled(){
+            setupLocationManager()
+            checkLocationAuthorization()
+        }else {
+            // show alert letting
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func checkLocationAuthorization(){
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerViewOnUserLoction()
+            locationManager.startUpdatingLocation()
+            break
+        case .denied:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        }
     }
-    */
-
 }
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
+    
+}
+
